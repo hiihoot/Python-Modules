f41f3703d5630c2077bccd7c57cd85d657ffe3df
hiihoot <hihoot@proton.me>
2026-02-02 11:52:24 +0100

    ex1-M01

---
+++ Python Module 01/ft_garden_intro.py
@@ -0,0 +1,12 @@
+
+if __name__ == "__main__":
+    print("=== Welcome to My Garden ===")
+    name = "Eggplant"
+    height = '12cm'
+    age = "30 days"
+    print(f"Plant: {name}")
+    print(f"Height: {height}")
+    print(f"Age: {age}")
+    print("=== End of Program ===")
+
+print(__name__, type(__name__))
