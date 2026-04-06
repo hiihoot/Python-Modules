a72fe4fd92885dc1041a66436192745f5ece378b
hihoot <hiihoot>
2026-04-06 14:23:29 +0100

    on progress: py8

--- Python Module 08/ex01/loading.py
+++ Python Module 08/ex01/loading.py
@@ -1,12 +1,28 @@
 import importlib
 import sys
 
-try:
-    pandas = importlib.import_module("pandas")
-    requests = importlib.import_module("requests")
-    matplotlib = importlib.import_module("matplotlib")
 
-    print(pandas)
+def main():
+    modules = ["pandas", "requests", "matplotlib"]
+    text = {
+        "pandas": "Data manipulation ready",
+        "requests": "Network access read",
+        "matplotlib": "Visualization ready"
+    }
 
-except ModuleNotFoundError:
-    pass
+    print("LOADING STATUS: Loading programs...\n")
+
+    for m in modules:
+        try:
+            module = importlib.import_module(m)
+            print(f"[OK] {module.__name__} ({module.__version__}) - "
+                  f"{text[m]}")
+        except ModuleNotFoundError:
+            print("Use 'install -r requirements.txt' or 'peotry install'")
+            break
+    
+    print(module)
+
+
+if __name__ == "__main__":
+    main()
